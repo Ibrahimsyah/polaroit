40d17c20415e2000786f0edc8176e192f65ad00a
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-07-22 15:34:34 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://ibrahimsyah.github.io/polaroit/sw.js', { scope: 'https://ibrahimsyah.github.io/polaroit/' })})}
